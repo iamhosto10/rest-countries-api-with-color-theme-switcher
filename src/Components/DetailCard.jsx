d9f40e67718dfba2cb36cc54f3f066907173d1dd
iamhosto10 <gerav0400@gmail.com>
2022-10-12 08:24:41 -0500

    settings of gh-pages

--- src/Components/DetailCard.jsx
+++ src/Components/DetailCard.jsx
@@ -1,25 +1,20 @@
-import React from 'react'
-import { Detail } from '../Cover/Cover.elements'
+import React from "react";
+import { Detail } from "../Cover/Cover.elements";
 
 function DetailCard() {
   return (
-      <>
-          <Detail>
-              <button>
-                  
-              </button>
-              <article className="Flex">
-                  <img> </img>
-                  <div className='detail-container'>
-                      <h2>
-                          
-                      </h2>
-
-                  </div>
-              </div>
-          </Detail>
-      </>
-  )
+    <>
+      <Detail>
+        <button></button>
+        <article className="Flex">
+          <img> </img>
+          <div className="detail-container">
+            <h2></h2>
+          </div>
+        </article>
+      </Detail>
+    </>
+  );
 }
 
-export default DetailCard
+export default DetailCard;
